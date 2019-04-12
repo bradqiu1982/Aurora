013898cc37bbb0bd22aa1ce8d2212d724ff9319e
Brad Qiu <brad.qiu@finisar.com>
2019-04-12 15:52:55 +0800

    add R.NET sample into project

---
+++ Aurora/Controllers/MainController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+using RDotNet;
+using System.Collections;
+using Aurora.Models;
+
+namespace Aurora.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+
+                //REngine.SetEnvironmentVariables();
+                REngine.SetEnvironmentVariables("C:/Rtools/R/R342/bin/i386", "C:/Rtools/R/R342");
+                // There are several options to initialize the engine, but by default the following suffice:
+                REngine engine = REngine.GetInstance();
+
+            //smaple 1 -- use statics function
+            //engine.Evaluate("library(stats)");
+            //engine.Evaluate("options(digits=2)");
+            //var colname = new string[] { "Student","Math","Science","English" };
+            //var Student = new string[] { "john dav","angela willia","bullwink moose","david jone","janice mark","chery cush","reuven ytz","greg knox","joel england","mary ray"};
+            //var Math = new int[] { 502,600,412,358,495,512,410,625,573,522};
+            //var Science = new int[] { 95,99,80,82,75,85,80,95,89,86};
+            //var English = new int[] { 25,22,18,15,20,28,15,30,27,18};
+            //IEnumerable[] srcdata = new IEnumerable[4];
+            //srcdata[0] = Student;
+            //srcdata[1] = Math;
+            //srcdata[2] = Science;
+            //srcdata[3] = English;
+            //var roster = engine.CreateDataFrame(srcdata, colname);
+
+            //engine.SetSymbol("roster", roster);
+            //engine.Evaluate("z <- scale(roster[,2:4])");
+            //engine.Evaluate("score <- apply(z,1,mean)");
+            //engine.Evaluate("roster <- cbind(roster,score)");
+            //engine.Evaluate("y <- quantile(score, c(.8,.6,.4,.2))");
+            //engine.Evaluate("roster$grad[score >= y[1]] <- \"A\"");
+            //engine.Evaluate("roster$grad[score < y[1] & score >= y[2]] <- \"B\"");
+            //engine.Evaluate("roster$grad[score < y[2] & score >= y[3]] <- \"C\"");
+            //engine.Evaluate("roster$grad[score < y[3] & score >= y[4]] <- \"D\"");
+            //engine.Evaluate("roster$grad[score < y[4]] <- \"F\"");
+            //engine.Evaluate("roster <- roster[order(roster$Student),]");
+
+            //roster = engine.GetSymbol("roster").AsDataFrame();
+
+            //var colnum = roster.ColumnCount;
+            //var rows =  roster.GetRows();
+            //foreach (DataFrameRow r in rows)
+            //{
+            //    var vlist = new List<object>();
+            //    for (var cl = 0; cl < colnum; cl++)
+            //    {
+            //        vlist.Add(r[cl]);
+            //    }
+            //    System.Windows.MessageBox.Show(string.Join(",", vlist));
+            //}
+
+            //sample2 -- generate plot
+            var plotfolder = this.Server.MapPath("~/userfiles") + "/docs/plot/";
+            CreateFolder(plotfolder);
+            var fn = Guid.NewGuid().ToString("N") + ".png";
+            var plotname = plotfolder + fn;
+            var url = "/userfiles/docs/plot/" + fn;
+
+            var does = new double[] { 20, 30, 40, 45, 60 };
+            var druga = new double[] { 16, 20, 27, 40, 60 };
+            var cdoes = engine.CreateNumericVector(does);
+            var cdruga = engine.CreateNumericVector(druga);
+
+            engine.Evaluate("library(graphics)");
+            engine.SetSymbol("does", cdoes);
+            engine.SetSymbol("draga", cdruga);
+            engine.Evaluate("png('" + plotname.Replace("\\", "/") + "')");
+            engine.Evaluate("plot(does,draga,type='b')");
+            engine.Evaluate("dev.off()");
+            engine.ClearGlobalEnvironment();
+            ViewBag.url = url;
+
+            return View();
+        }
+
+        private void CreateFolder(string folder)
+        {
+            if (!System.IO.Directory.Exists(folder))
+            { System.IO.Directory.CreateDirectory(folder); }
+        }
+    }
+}
---
+++ Aurora/Models/SysAuthorization.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Xml;
+
+namespace Aurora.Models
+{
+    public class SysAuthorization : IDisposable
+    {
+        // obtains user token  
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+
+        static extern bool LogonUser(string pszUsername, string pszDomain, string pszPassword,
+
+            int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+
+
+
+        // closes open handes returned by LogonUser  
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+
+        extern static bool CloseHandle(IntPtr handle);
+        [DllImport("Advapi32.DLL")]
+        static extern bool ImpersonateLoggedOnUser(IntPtr hToken);
+        [DllImport("Advapi32.DLL")]
+        static extern bool RevertToSelf();
+        const int LOGON32_PROVIDER_DEFAULT = 0;
+        const int LOGON32_LOGON_NEWCREDENTIALS = 2;
+
+        private bool disposed;
+
+        public SysAuthorization(Controller ctrl)
+        {
+            var syscfg = CfgUtility.GetSysConfig(ctrl);
+            var sUsername = syscfg["SHAREFOLDERUSER"];
+            var sDomain = syscfg["SHAREFOLDERDOMIN"];
+            var sPassword = syscfg["SHAREFOLDERPWD"];
+
+            // initialize tokens  
+
+            IntPtr pExistingTokenHandle = new IntPtr(0);
+            IntPtr pDuplicateTokenHandle = new IntPtr(0);
+            try
+            {
+                // get handle to token  
+                bool bImpersonated = LogonUser(sUsername, sDomain, sPassword,
+
+                    LOGON32_LOGON_NEWCREDENTIALS, LOGON32_PROVIDER_DEFAULT, ref pExistingTokenHandle);
+                if (true == bImpersonated)
+                {
+
+                    if (!ImpersonateLoggedOnUser(pExistingTokenHandle))
+                    {
+                        int nErrorCode = Marshal.GetLastWin32Error();
+                        throw new Exception("ImpersonateLoggedOnUser error;Code=" + nErrorCode);
+                    }
+                }
+                else
+                {
+                    int nErrorCode = Marshal.GetLastWin32Error();
+                    throw new Exception("LogonUser error;Code=" + nErrorCode);
+                }
+
+            }
+
+            finally
+            {
+                // close handle(s)  
+                if (pExistingTokenHandle != IntPtr.Zero)
+                    CloseHandle(pExistingTokenHandle);
+                if (pDuplicateTokenHandle != IntPtr.Zero)
+                    CloseHandle(pDuplicateTokenHandle);
+            }
+
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+
+            if (!disposed)
+            {
+                RevertToSelf();
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
